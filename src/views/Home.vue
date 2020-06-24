d5338758c535d71630ab5210a0b46fd143cf226f
Jose Maza <mazajose@gmail.com>
2020-06-24 12:48:21 +0200

    Added Home Views Group

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,39 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <v-container>
+    <!-- BANNER -->
+    <v-row align="center" justify="center" class="green white--text pt-8">
+      <h1 class="h1 text-h4 font-weight-black">conduit</h1>
+    </v-row>
+    <v-row align="center" justify="center" class="green white--text pt-2 pb-5">
+      <p class="text-body-1">A place to share your knowledge.</p>
+    </v-row>
+
+    <!-- Page -->
+    <v-tabs>
+      <v-tab>
+        <router-link :to="{ name: 'home-my-feed' }">Your Feed</router-link>
+      </v-tab>
+      <v-tab>
+        <router-link :to="{ name: 'home' }" exact>Global Feed</router-link>
+      </v-tab>
+      <v-tab>
+        <!--<router-link :to="{ name: 'home-tag', params: { tag } }">
+          <i class="ion-pound"></i>
+          {{ tag }}
+        </router-link>-->
+      </v-tab>
+    </v-tabs>
+  </v-container>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+//import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    //HelloWorld
   }
 };
 </script>
